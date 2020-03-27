6fc774003a9b3b78cbe5920811e270e1acd5d102
aligator <aligator@suncraft-server.de>
2020-03-27 19:58:49 +0100

    :sparkles: add game over message

--- entities/hud.go
+++ entities/hud.go
@@ -27,7 +27,21 @@ func (h *Hud) Update() error {
 	return nil
 }
 
+func textDimension(text string) (w int, h int) {
+	return 7 * len(text), 13
+}
+
 func (h *Hud) Render(screen *ebiten.Image) error {
 	text.Draw(screen, "Score: "+strconv.Itoa(h.game.points), basicfont.Face7x13, 20, 20, color.White)
+
+	if !h.game.running {
+		gameOverText := "GAME OVER"
+		textW, textH := textDimension(gameOverText)
+		screenW := screen.Bounds().Dx()
+		screenH := screen.Bounds().Dy()
+
+		text.Draw(screen, gameOverText, basicfont.Face7x13, screenW/2-textW/2, screenH/2+textH/2, color.White)
+	}
+
 	return nil
 }
